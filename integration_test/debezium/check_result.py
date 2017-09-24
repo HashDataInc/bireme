3d90c1a845c0bfc5f3909b058de42f709dd9c6eb
Yuze Zhou <zhouyuze@hashdata.cn>
2017-09-24 11:58:59 +0800

    print diff

--- integration_test/debezium/check_result.py
+++ integration_test/debezium/check_result.py
@@ -4,28 +4,32 @@ sys.path.append(os.environ["TEST_DIR"])
 from sqldump import sqldump
 
 with open('source.txt', 'w') as f:
-	table = sqldump("postgres","127.0.0.1", 5432, "postgres", "postgres", "postgres", "binarysource", "id")
-	f.write(table)
-	table = sqldump("postgres","127.0.0.1", 5432, "postgres", "postgres", "postgres", "charsource", "id")
-	f.write(table)
-	table = sqldump("postgres","127.0.0.1", 5432, "postgres", "postgres", "postgres", "numericsource", "id")
-	f.write(table)
-	table = sqldump("postgres","127.0.0.1", 5432, "postgres", "postgres", "postgres", "timesource", "id")
-	f.write(table)
-	
+        table = sqldump("postgres","127.0.0.1", 5432, "postgres", "postgres", "postgres", "binarysource", "id")
+        f.write(table)
+        table = sqldump("postgres","127.0.0.1", 5432, "postgres", "postgres", "postgres", "charsource", "id")
+        f.write(table)
+        table = sqldump("postgres","127.0.0.1", 5432, "postgres", "postgres", "postgres", "numericsource", "id")
+        f.write(table)
+        table = sqldump("postgres","127.0.0.1", 5432, "postgres", "postgres", "postgres", "timesource", "id")
+        f.write(table)
+        
 with open('target.txt', 'w') as f:
-	table = sqldump("postgres","127.0.0.1", 5432, "postgres", "postgres", "postgres", "binarytarget", "id")
-	f.write(table)
-	table = sqldump("postgres","127.0.0.1", 5432, "postgres", "postgres", "postgres", "chartarget", "id")
-	f.write(table)
-	table = sqldump("postgres","127.0.0.1", 5432, "postgres", "postgres", "postgres", "numerictarget", "id")
-	f.write(table)
-	table = sqldump("postgres","127.0.0.1", 5432, "postgres", "postgres", "postgres", "timetarget", "id")
-	f.write(table)
-	
+        table = sqldump("postgres","127.0.0.1", 5432, "postgres", "postgres", "postgres", "binarytarget", "id")
+        f.write(table)
+        table = sqldump("postgres","127.0.0.1", 5432, "postgres", "postgres", "postgres", "chartarget", "id")
+        f.write(table)
+        table = sqldump("postgres","127.0.0.1", 5432, "postgres", "postgres", "postgres", "numerictarget", "id")
+        f.write(table)
+        table = sqldump("postgres","127.0.0.1", 5432, "postgres", "postgres", "postgres", "timetarget", "id")
+        f.write(table)
+        
 with open("source.txt") as source:
-   with open("target.txt") as target:
-      if source.read() == target.read():
-      	exit(0)
-      else:
-      	exit(1)
+	with open("target.txt") as target:
+		sourceContent = source.read()
+		targetContent = target.read()
+		if sourceContent == targetContent:
+			exit(0)
+		else:
+			print "Source:\n" + sourceContent
+			print "Target:\n" + targetContent
+			exit(1)
--- integration_test/maxwell/check_result.py
+++ integration_test/maxwell/check_result.py
@@ -4,28 +4,28 @@ sys.path.append(os.environ["TEST_DIR"])
 from sqldump import sqldump
 
 with open('source.txt', 'w') as f:
-	table = sqldump("mysql","127.0.0.1", 3306, "root", "123456", "demo", "binarysource", "id")
-	f.write(table)
-	table = sqldump("mysql","127.0.0.1", 3306, "root", "123456", "demo", "charsource", "id")
-	f.write(table)
-	table = sqldump("mysql","127.0.0.1", 3306, "root", "123456", "demo", "numericsource", "id")
-	f.write(table)
-	table = sqldump("mysql","127.0.0.1", 3306, "root", "123456", "demo", "timesource", "id")
-	f.write(table)
-	
+    table = sqldump("mysql","127.0.0.1", 3306, "root", "123456", "demo", "binarysource", "id")
+    f.write(table)
+    table = sqldump("mysql","127.0.0.1", 3306, "root", "123456", "demo", "charsource", "id")
+    f.write(table)
+    table = sqldump("mysql","127.0.0.1", 3306, "root", "123456", "demo", "numericsource", "id")
+    f.write(table)
+    table = sqldump("mysql","127.0.0.1", 3306, "root", "123456", "demo", "timesource", "id")
+    f.write(table)
+    
 with open('target.txt', 'w') as f:
-	table = sqldump("postgres","127.0.0.1", 5432, "postgres", "postgres", "postgres", "binarytarget", "id")
-	f.write(table)
-	table = sqldump("postgres","127.0.0.1", 5432, "postgres", "postgres", "postgres", "chartarget", "id")
-	f.write(table)
-	table = sqldump("postgres","127.0.0.1", 5432, "postgres", "postgres", "postgres", "numerictarget", "id")
-	f.write(table)
-	table = sqldump("postgres","127.0.0.1", 5432, "postgres", "postgres", "postgres", "timetarget", "id")
-	f.write(table)
-	
+    table = sqldump("postgres","127.0.0.1", 5432, "postgres", "postgres", "postgres", "binarytarget", "id")
+    f.write(table)
+    table = sqldump("postgres","127.0.0.1", 5432, "postgres", "postgres", "postgres", "chartarget", "id")
+    f.write(table)
+    table = sqldump("postgres","127.0.0.1", 5432, "postgres", "postgres", "postgres", "numerictarget", "id")
+    f.write(table)
+    table = sqldump("postgres","127.0.0.1", 5432, "postgres", "postgres", "postgres", "timetarget", "id")
+    f.write(table)
+    
 with open("source.txt") as source:
-   with open("target.txt") as target:
-      if source.read() == target.read():
-      	exit(0)
-      else:
-      	exit(1)
+    with open("target.txt") as target:
+        if source.read() == target.read():
+            exit(0)
+        else:
+        	exit(1)
