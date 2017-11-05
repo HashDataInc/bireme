edc0eddce96b94220e95d4c766a989b58772d9bd
Yuze Zhou <zhouyuze@hashdata.cn>
2017-11-05 10:50:26 +0800

    modify python script to select in lexicographic order

--- integration_test/sqldump.py
+++ integration_test/sqldump.py
@@ -4,38 +4,39 @@ import binascii
 import re
 from cStringIO import StringIO
 
-def sqldump(dbtype, host, port, user, passwd, db, table, *key):
+def sqlDump(dbtype, host, port, user, passwd, db, table, *key):
     try:
-        connection = getconnection(dbtype, host, port, user, passwd, db)
+        connection = getConnection(dbtype, host, port, user, passwd, db)
         dbhandler = connection.cursor()
-        types = gettypes(dbhandler, dbtype, table)
-        dbhandler.execute("SELECT * FROM "+table +" order by "+" ".join(key))
+        nameAndType = getNameAndType(dbhandler, dbtype, table)
+        nameAndType.sort()
+
+        names = [line[0] for line in nameAndType]
+        types = [line[1] for line in nameAndType]
+
+        dbhandler.execute("SELECT " + ", ".join(names) + " FROM " + table + " order by " + ", ".join(key))
         
         if dbtype.lower() == "mysql":
-            return mysqldump(dbhandler, types)
+            return mysqlDump(dbhandler, types)
         elif dbtype.lower() == "postgres":
-            return pgdump(dbhandler, types)
+            return pgDump(dbhandler, types)
 
     except Exception as e:
         print e
     finally:
         connection.close()
 
-def getconnection(dbtype, host, port, user, passwd, db):
+def getConnection(dbtype, host, port, user, passwd, db):
     if dbtype.lower() == "mysql":
         return MySQLdb.connect(host=host, port=port, user=user, passwd=passwd, db=db)
     else:
         return psycopg2.connect(host=host, port=port, user=user, password=passwd, dbname=db)
 
-def gettypes(dbhandler, dbtype, table):
-    if dbtype.lower() == "mysql":
-        dbhandler.execute("desc "+table)
-        return [line[1] for line in dbhandler.fetchall()]
-    elif dbtype.lower() == "postgres":
-        dbhandler.execute("SELECT data_type FROM information_schema.columns WHERE table_name='"+table+"' ORDER BY ordinal_position")
-        return [line[0] for line in dbhandler.fetchall()]
+def getNameAndType(dbhandler, dbtype, table):
+    dbhandler.execute("SELECT column_name, data_type FROM information_schema.columns WHERE table_name='" + table + "' ORDER BY column_name")
+    return [line for line in dbhandler.fetchall()]
 
-def mysqldump(dbhandler, types):
+def mysqlDump(dbhandler, types):
     length = range(0, len(types))
     fileStr = StringIO()
 
@@ -46,16 +47,18 @@ def mysqldump(dbhandler, types):
 
         for i in length:
             if line[i] is None:
-                fileStr.write(str(line[i])+"\t")
+                fileStr.write(str(line[i]) + "\t")
             elif re.search("binary.*|bit.*|blob.*", types[i]):
-                fileStr.write(str(binascii.hexlify(line[i]))+"\t")
+                fileStr.write(str(binascii.hexlify(line[i])) + "\t")
+            elif re.search("decimal", types[i]):
+                fileStr.write(str(line[i].normalize()) + "\t")
             else:
-                fileStr.write(str(line[i])+"\t")
+                fileStr.write(str(line[i]) + "\t")
         fileStr.write("\n")
 
     return fileStr.getvalue()
 
-def pgdump(dbhandler, types):
+def pgDump(dbhandler, types):
     length = range(0, len(types))
     fileStr = StringIO()
 
@@ -66,15 +69,17 @@ def pgdump(dbhandler, types):
 
         for i in length:
             if line[i] is None:
-                fileStr.write(str(line[i])+"\t")
+                fileStr.write(str(line[i]) + "\t")
             elif re.search("bytea", types[i]):
-                fileStr.write(str(binascii.hexlify(line[i]))+"\t")
+                fileStr.write(str(binascii.hexlify(line[i])) + "\t")
             elif re.search("bit", types[i]):
-                fileStr.write(str(hex(int(line[i], 2))[2:])+"\t")
+                fileStr.write(str(hex(int(line[i], 2))[2:]) + "\t")
             elif re.search("boolean", types[i]):
                 fileStr.write("1\t" if line[i] else "0\t")
+            elif re.search("numeric", types[i]):
+                fileStr.write(str(line[i].normalize()) + "\t")
             else:
-                fileStr.write(str(line[i])+"\t")
+                fileStr.write(str(line[i]) + "\t")
         fileStr.write("\n")
 
     return fileStr.getvalue()
