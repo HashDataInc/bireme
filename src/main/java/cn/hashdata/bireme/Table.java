5728c0861e76677b65f6c4412d8f1f524c02ca30
sunguofeng <sunguofeng@yuantiaotech.com>
2020-12-01 16:34:48 +0800

    Update Table.java
    public schema的限制

--- src/main/java/cn/hashdata/bireme/Table.java
+++ src/main/java/cn/hashdata/bireme/Table.java
@@ -57,14 +57,15 @@ public class Table {
     ResultSetMetaData rsMetaData = null;
 
     try {
-      List<String> mapList = tableMap.get(tableName);
+      String[] strArray = tableName.split("\\.");
+      List<String> mapList = tableMap.get(strArray[1]);
       for (int i = 0; i < mapList.size(); i++) {
         this.keyNames.add(mapList.get(i));
       }
 
       statement = conn.createStatement();
 
-      String queryTableInfo = "select * from public." + tableName + " where 1=2";
+      String queryTableInfo = "select * from " + tableName + " where 1=2";
       rs = statement.executeQuery(queryTableInfo);
       rsMetaData = rs.getMetaData();
       this.ncolumns = rsMetaData.getColumnCount();
