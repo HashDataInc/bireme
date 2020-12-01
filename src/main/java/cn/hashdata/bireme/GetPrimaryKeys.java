38a179c52a97adc81578d9e8d02fa7bff7be280b
sunguofeng <sunguofeng@yuantiaotech.com>
2020-12-01 14:59:52 +0800

    Update GetPrimaryKeys.java
    主键和表初始化检查时，解决只能使用public schema的问题

--- src/main/java/cn/hashdata/bireme/GetPrimaryKeys.java
+++ src/main/java/cn/hashdata/bireme/GetPrimaryKeys.java
@@ -29,12 +29,11 @@ public class GetPrimaryKeys {
     sb.append("(");
 
     for (String fullname : tableMap.values()) {
-      strArray = fullname.split("\\.");
-      sb.append("'").append(strArray[1]).append("',");
+      sb.append("'").append(fullname).append("',");
     }
 
     String tableList = sb.toString().substring(0, sb.toString().length() - 1) + ")";
-    String tableSql = "select tablename from pg_tables where schemaname='public' and tablename in "
+    String tableSql = "select tablename from pg_tables where concat_ws('.',schemaname,tablename) in "
         + tableList + "";
     String prSql = "SELECT NULL AS TABLE_CAT, "
         + "n.nspname  AS TABLE_SCHEM, "
@@ -45,7 +44,7 @@ public class GetPrimaryKeys {
         + "FROM pg_catalog.pg_class ct JOIN pg_catalog.pg_attribute a ON (ct.oid = a.attrelid) "
         + "JOIN pg_catalog.pg_namespace n ON (ct.relnamespace = n.oid) "
         + "JOIN ( SELECT i.indexrelid, i.indrelid, i.indisprimary, information_schema._pg_expandarray(i.indkey) AS KEYS FROM pg_catalog.pg_index i) i ON (a.attnum = (i.keys).x AND a.attrelid = i.indrelid) "
-        + "JOIN pg_catalog.pg_class ci ON (ci.oid = i.indexrelid) WHERE TRUE AND n.nspname = 'public' AND ct.relname in "
+        + "JOIN pg_catalog.pg_class ci ON (ci.oid = i.indexrelid) WHERE TRUE AND concat_ws('.',n.nspname,ct.relname) in "
         + tableList + " AND i.indisprimary ORDER BY TABLE_NAME, pk_name, key_seq";
     try {
       statement = conn.createStatement();
